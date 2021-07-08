bc459b63ca8e90d2d0216bbe85427a7829f52877
LWRGitHub <LoganReynoldsLWR@gmail.com>
2021-07-07 22:52:03 -0700

    API working & data is on web page

--- server.js
+++ server.js
@@ -32,26 +32,6 @@
   
   require('./routes/index.js')(app);
 
-  
-  // catch 404 and forward to error handler
-  app.use((req, res, next) => {
-    const err = new Error('Not Found');
-    err.status = 404;
-    console.log(err)
-    // next(err);
-  });
-  
-  // error handler
-  app.use((err, req, res, next) => {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
-  
-    // render the error page
-    res.status(err.status || 500);
-    res.render('error');
-  });
-
   /* istanbul ignore next */
 if (!module.parent) {
     app.listen(3000);
