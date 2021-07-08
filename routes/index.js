6d11be1a2490dc2ead9bf9fbac96962007d08fa5
LWRGitHub <LoganReynoldsLWR@gmail.com>
2021-07-07 20:33:32 -0700

    working on API call

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,27 @@
+const request = require('request');
+
 module.exports = (app) => {
 
   /* GET home page. */
   // with pagination
   app.get('/', (req, res) => {
 
+    const signs = ["libra", "aquarius", "pisces", "aries", "taurus", "gemini", "cancer", "leo", "virgo", "scorpio", "sagittarius", "capricorn"]
+    let signDatas = []
 
+    for(let i = 0; i < signs.length; i++){
+      request(`https://ohmanda.com/api/horoscope/${signs[i]}`, function (err, res, body) {
+        if (!err && res.statusCode == 200) {
+          const signObj = JSON.parse(body)
+          // console.log(signObj)
+          signDatas.push(signObj)
+          // console.log(signDatas)
+        }
+      })
+    }
     
-    res.render('index', {  });
+    console.log("------HERE---------",signDatas)
+    res.render('index', { signDatas: signDatas });
 
   });
 
